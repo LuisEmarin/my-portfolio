194f258ffa4cb469bd6321012ccc90e0833902a8
luisEmarin <Luis.e.marin@outlook.com>
2022-02-11 12:25:40 -0500

    fixed

--- src/data.js
+++ src/data.js
@@ -5,14 +5,14 @@ export const projects = [
     description:
       "Using a Spotify API I conconcted 'wall' of movie posters that you can click on, and will play a snippet of the music used in the movie.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://eloquent-villani-72b992.netlify.app/",
   },
   {
     title: "SimplyShop",
     subtitle: "React, Google Firebase, API, JavaScript",
     description:
-      "An E-commerce application that has the ability to gather and display data (CRUD) using a third party WEB API, Sign up and Sign IN/OUT, and provides a user-friendly interface.",
-    image: "./project-2.gif",
+      "An E-commerce application that has the ability to gather/display data (CRUD) using a third party WEB API, Sign up, Sign IN/OUT, and provides a user-friendly interface.",
+    image: "./giphy.gif",
     link: "https://e-commerce-cf99a.web.app/App",
   },
   {
@@ -20,15 +20,15 @@ export const projects = [
     subtitle: "JavaScript and API's",
     description:
       "Using the Open Weather API from openweathermap.org I created a weather application that displays the High, Low, Forecast and Humidity using JavaScript",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    image: "./giphy-3.gif",
+    link: "https://compassionate-pike-e42c68.netlify.app/",
   },
   {
     title: "Drones Inventory",
     subtitle: "Firebase Hosting, SQL Database, Google Authentication",
     description:
       "A site with data from a fabricated Flask API. API calls written as JS/TS functions. Implemented custom hooks are used to call the functions on the fly! Once data is sent back, you can create/retrieve/update/delete directly from the Dashboard/DataGrid Components.",
-    image: "./project-4.gif",
+    image: "./giphy-2.gif",
     link: "https://drones-73.web.app/",
   },
 ];
